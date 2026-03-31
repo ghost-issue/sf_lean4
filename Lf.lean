2871a1faaf192e1d262fa789e40698d66053d377
ghost <230292698+ghost-issue@users.noreply.github.com>
2026-03-31 21:54:51 +0800

    update

--- Lf.lean
+++ Lf.lean
@@ -1,1 +1,5 @@
-import Lf.Basic
+import lf.Basics
+import lf.Induction
+import lf.Lists
+import lf.Poly
+import lf.Tactics
---
+++ Test.lean
@@ -0,0 +1,8 @@
+import lf.Basics
+
+namespace LF
+
+theorem test1 (n : Nat) : n + 0 = n := rfl
+theorem test2 (n : Nat) : 0 + n = n := rfl
+
+end LF
